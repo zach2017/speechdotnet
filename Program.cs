d98be6987c302e7cfa0833f07f8367960f746cc3
Zach Lewis <zaclewis@gmail.com>
2023-12-13 13:40:03 -0600

    add code

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string subscriptionKey = "f9f93333f3fa42b4ba4b419401c335da";
+string subscriptionRegion = "eastus";
+
+var config = SpeechConfig.FromSubscription(subscriptionKey, subscriptionRegion);
+// Note: the voice setting will not overwrite the voice element in input SSML.
+config.SpeechSynthesisVoiceName = "en-US-DavisNeural";
+
+string text = "What is your name  ? ";
+
+// use the default speaker as audio output.
+using (var synthesizer = new SpeechSynthesizer(config))
+{
+    using (var result = await synthesizer.SpeakTextAsync(text))
+    {
+        if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+        {
+            Console.WriteLine($"Speech synthesized for text [{text}]");
+        }
+        else if (result.Reason == ResultReason.Canceled)
+        {
+            var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+            Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+            if (cancellation.Reason == CancellationReason.Error)
+            {
+                Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                Console.WriteLine($"CANCELED: Did you update the subscription info?");
+            }
+        }
+    }
+}
+
+
+ using (var recognizer = new SpeechRecognizer(config))
+{
+    Console.WriteLine("Say something...");
+
+    // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+    // single utterance is determined by listening for silence at the end or until a maximum of 15
+    // seconds of audio is processed.  The task returns the recognition text as result. 
+    // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+    // shot recognition like command or query. 
+    // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+    var result = await recognizer.RecognizeOnceAsync();
+
+    // Checks result.
+    if (result.Reason == ResultReason.RecognizedSpeech)
+    {
+        Console.WriteLine($"We recognized: {result.Text}");
+    }
+    else if (result.Reason == ResultReason.NoMatch)
+    {
+        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+    }
+    else if (result.Reason == ResultReason.Canceled)
+    {
+        var cancellation = CancellationDetails.FromResult(result);
+        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+        if (cancellation.Reason == CancellationReason.Error)
+        {
+            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+        }
+    }
+
+}
