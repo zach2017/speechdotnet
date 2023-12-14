0e5a9f0cc091398dad7503fd1080636eacd1a0bb
Zach Lewis <zaclewis@gmail.com>
2023-12-14 16:48:29 -0600

    WIP Form Speech

--- Program.cs
+++ Program.cs
@@ -4,76 +4,126 @@ using System.Threading.Tasks;
 using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
 
+Console.WriteLine("Speech Form Entry");
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-string subscriptionKey = "f9f93333f3fa42b4ba4b419401c335da";
+string subscriptionKey = Environment.GetEnvironmentVariable("AZURE_SPEECH_KEY") ?? "Default_key";
 string subscriptionRegion = "eastus";
 
 var config = SpeechConfig.FromSubscription(subscriptionKey, subscriptionRegion);
 // Note: the voice setting will not overwrite the voice element in input SSML.
 config.SpeechSynthesisVoiceName = "en-US-DavisNeural";
 
-string text = "What is your name  ? ";
+string text = "Welcome to the Speech Form Entry Application";
+string text2 = "What is your name  ? ";
 
 // use the default speaker as audio output.
-using (var synthesizer = new SpeechSynthesizer(config))
+try
 {
-    using (var result = await synthesizer.SpeakTextAsync(text))
+    using (var synthesizer = new SpeechSynthesizer(config))
     {
-        if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+        using (var result = await synthesizer.SpeakTextAsync(text))
         {
-            Console.WriteLine($"Speech synthesized for text [{text}]");
-        }
-        else if (result.Reason == ResultReason.Canceled)
-        {
-            var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
-            Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
-
-            if (cancellation.Reason == CancellationReason.Error)
+            if (result.Reason == ResultReason.SynthesizingAudioCompleted)
             {
-                Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-                Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
-                Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                Console.WriteLine($"Speech synthesized for text [{text}]");
+            }
+            else if (result.Reason == ResultReason.Canceled)
+            {
+                var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                    Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                }
+            }
+        }
+
+
+        string filePath = "formquestions.txt";
+        string filePath2 = "formanswers.txt";
+
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+
+
+                using (var result = await synthesizer.SpeakTextAsync(line))
+                {
+                    if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+                    {
+                        Console.WriteLine($"Speech synthesized for text [{line}]");
+                    }
+                    else if (result.Reason == ResultReason.Canceled)
+                    {
+                        var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                    }
+                }
+
+
+                using (var recognizer = new SpeechRecognizer(config))
+                {
+                    Console.WriteLine("Say something...");
+
+                    // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+                    // single utterance is determined by listening for silence at the end or until a maximum of 15
+                    // seconds of audio is processed.  The task returns the recognition text as result. 
+                    // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+                    // shot recognition like command or query. 
+                    // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+                    var result = await recognizer.RecognizeOnceAsync();
+
+                    // Checks result.
+                    if (result.Reason == ResultReason.RecognizedSpeech)
+                    {
+                        using (StreamWriter writer = new StreamWriter(filePath2, true))
+                        {
+                            Console.WriteLine($"We recognized: {result.Text}");
+                            writer.WriteLine(line + " = " + result.Text);
+                        }
+
+                    }
+                    else if (result.Reason == ResultReason.NoMatch)
+                    {
+                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    }
+                    else if (result.Reason == ResultReason.Canceled)
+                    {
+                        var cancellation = CancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                    }
+
+
+                }
+
             }
         }
     }
+
 }
-
-
- using (var recognizer = new SpeechRecognizer(config))
+catch (Exception e)
 {
-    Console.WriteLine("Say something...");
-
-    // Starts speech recognition, and returns after a single utterance is recognized. The end of a
-    // single utterance is determined by listening for silence at the end or until a maximum of 15
-    // seconds of audio is processed.  The task returns the recognition text as result. 
-    // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
-    // shot recognition like command or query. 
-    // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
-    var result = await recognizer.RecognizeOnceAsync();
-
-    // Checks result.
-    if (result.Reason == ResultReason.RecognizedSpeech)
-    {
-        Console.WriteLine($"We recognized: {result.Text}");
-    }
-    else if (result.Reason == ResultReason.NoMatch)
-    {
-        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
-    }
-    else if (result.Reason == ResultReason.Canceled)
-    {
-        var cancellation = CancellationDetails.FromResult(result);
-        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
-
-        if (cancellation.Reason == CancellationReason.Error)
-        {
-            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
-            Console.WriteLine($"CANCELED: Did you update the subscription info?");
-        }
-    }
-
+    Console.WriteLine("ERROR: Could not start speech services ..");
+    Console.WriteLine(e.ToString());
 }
